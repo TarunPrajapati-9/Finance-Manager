58e50beb493b58c0b1edf476fd3d0eea6743fffc
Tarun Prajapati <tarun.prajapati912@gmail.com>
2024-01-30 17:29:09 +0530

    Ui Update

--- public/app.js
+++ public/app.js
@@ -11,9 +11,16 @@ const ul = document.querySelector("ul");
 const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
+    //first way
+    // type.value === "invoice"
+    //   ? (doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber))
+    //   : (doc = new Payment(tofrom.value, details.value, amount.valueAsNumber));
+    //second way
     type.value === "invoice"
-        ? (doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber))
-        : (doc = new Payment(tofrom.value, details.value, amount.valueAsNumber));
+        ? (doc = new Invoice(...values))
+        : (doc = new Payment(...values));
     list.render(doc, type.value, "end");
 });
--- src/app.ts
+++ src/app.ts
@@ -17,10 +17,19 @@ const list = new ListTemplate(ul);
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
+
   let doc: HasFormatter;
+  //first way
+  // type.value === "invoice"
+  //   ? (doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber))
+  //   : (doc = new Payment(tofrom.value, details.value, amount.valueAsNumber));
+
+  //second way
   type.value === "invoice"
-    ? (doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber))
-    : (doc = new Payment(tofrom.value, details.value, amount.valueAsNumber));
+    ? (doc = new Invoice(...values))
+    : (doc = new Payment(...values));
 
   list.render(doc, type.value, "end");
 });
